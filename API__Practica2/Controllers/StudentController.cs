fcc678a4e3815d4c561c01c2f3abcf3162b83141
Richard20175275 <51683907+Richard20175275@users.noreply.github.com>
2019-06-21 04:56:55 -0400

    Terminado
    Ya tiene todo el codigo, fuente (Service, Presistence, Model, etc)

---
+++ API__Practica2/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+using Model;
+
+namespace API__Practica2.Controllers
+{
+    [Route("controller")]
+    public class StudentController : Controller
+    {
+
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return OK(_studentService.GetAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return OK(_studentService.Get());
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Student model)
+        {
+            return OK(_studentService.Add(model));
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Student model)
+        {
+            return OK(_studentService.Add(model));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return OK(_studentService.Delete(model));
+        }
+    }
+}
